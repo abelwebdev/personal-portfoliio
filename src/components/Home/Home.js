2979e001ce0a3fa1d1164512541b65b6801bb87e
abelwebdev <82320458+abelwebdev@users.noreply.github.com>
2024-07-12 01:41:43 +0300

    rev 1

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,38 +2,18 @@ import React, {useState} from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import logo from "../../Assets/logo.png";
-import Button from "react-bootstrap/Button";
 import { Link, useNavigate } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
-import {
-  AiFillStar,
-  AiOutlineHome,
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
-} from "react-icons/ai";
-import { CgFileDocument } from "react-icons/cg";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Type from "./Type";
 import myImg from "../../Assets/avatar.svg";
-import Tilt from "react-parallax-tilt";
-import About from "../About/About.js";
-import Projects from "../Projects/Projects";
-import Footer from "../Footer";
-import Github from "../About/Github";
 import Techstack from "../About/Techstack";
-import Aboutcard from "../About/AboutCard";
-import laptopImg from "../../Assets/about.png";
-import Toolstack from "../About/Toolstack";
 import ProjectCard from "../Projects/ProjectCards";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import Footer from '../Footer'
 function Home() {
   const [expand, updateExpanded] = useState(false);
   const [navColour, updateNavbar] = useState(false);
@@ -52,9 +32,6 @@ function Home() {
       document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
       updateExpanded(false);
   };
-  const goToSection = (sectionId) => {
-      navigate(`#${sectionId}`);
-  };
   return (
     <section>
       <Navbar
@@ -64,9 +41,6 @@ function Home() {
         className={navColour ? "sticky" : "navbar"} 
       >
         <Container>
-          <Navbar.Brand href="/" className="d-flex">
-            <img src={logo} className="img-fluid logo" alt="brand" />
-          </Navbar.Brand>
           <Navbar.Toggle
             aria-controls="responsive-navbar-nav"
             onClick={() => {
@@ -139,7 +113,7 @@ function Home() {
                 </span>
               </h1>
               <h1 className="heading-name">
-                I'M
+                I'm
                 <strong className="main-name"> Abel </strong>
               </h1>
               <div style={{ padding: 50, textAlign: "left" }}>
@@ -199,9 +173,9 @@ function Home() {
               </p>
             </Col>
             <Col md={4} className="myAvtar">
-              <Tilt>
+              <div>
                 <img src={myImg} className="img-fluid" alt="avatar" />
-              </Tilt>
+              </div>
             </Col>
           </Row>
         </Container>
@@ -213,89 +187,51 @@ function Home() {
             Professional <strong className="purple">Skillset </strong>
           </h1>
           <Techstack />
-          <h1 className="project-heading">
-            <strong className="purple">Tools</strong> I use
-          </h1>
-          <Toolstack />
         </Container>
       </Container>
       <Container fluid className="project-section" id="projects">
-      <Particle />
-      <Container>
-        <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
-        </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
+        <Particle />
+        <Container>
+          <h1 className="project-heading">
+            My Recent <strong className="purple">Works </strong>
+          </h1>
+          <p style={{ color: "white" }}>
+            Here are a few projects I've worked on recently.
+          </p>
+          <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={chatify}
+                title="Chatify"
+                description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+                ghLink="https://github.com/soumyajit4419/Chatify"
+                demoLink="https://chatify-49.web.app/"
+              />
+            </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={bitsOfCode}
+                title="Bits-0f-C0de"
+                description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+                ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
+                demoLink="https://blogs.soumya-jit.tech/"
+              />
+            </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-            />
-          </Col>
-        </Row>
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={editor}
+                title="Editor.io"
+                description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+                ghLink="https://github.com/soumyajit4419/Editor.io"
+                demoLink="https://editor.soumya-jit.tech/"              
+              />
+            </Col>
+          </Row>
+        </Container>
       </Container>
-      </Container>
+      {/* <Footer /> */}
     </section>
   );
 }
